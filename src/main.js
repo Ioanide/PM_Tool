b629487d9a31875f15280c5a6cd0c85535e5766f
Ioan B <44924527+Ioanide@users.noreply.github.com>
2019-02-21 14:13:40 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,39 +1,84 @@
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+//document.getElementById('issueInputForm').addEventListener('submit', saveIssue());
+
+var firstID=1;
+
+function Issue(id, name, type, sprint, createdBy, assignedTo, description, status, tasks, comments, updatedAt, createdAt) {
+      this.id = firstID++;
+      this.name = name;
+      this.sprint = sprint;
+      this.createdBy = createdBy;
+      this.assignedTo = assignedTo;
+      this.description = description;
+      this.status = status;
+      this.tasks= tasks;
+      this.comments = comments;
+      this.updatedAt = updatedAt;
+      this.createdAt = createdAt;
+      this.type = type;
+  }
+
+  function Issue(name, type, description, status, assignedTo){
+    this.id = firstID++;
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.assignedTo = assignedTo;
+    this.status = status;
+  }
+
+  function getID() {
+      return this.id;
+  }
+
+  function getName(){
+    return this.name;
+  }
+  function getStatus(){
+    return this.status;
+  }
+  function getDescription(){
+    return this.description;
+  }
+  function getType(){
+    return this.type;
+  }
+  function getAssignedTo(){
+    return this.assignedTo;
+  }
+
+const status = ["New", "In progress", "Feedback", "Rework", "Resolved", "Ready for Testing"]
 
 function saveIssue() {
   let issueDesc = document.getElementById('issueDescInput').value;
   let issueType = document.getElementById('issueTypeInput').value;
   let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  let issueId = chance.guid();
+  //let issueId = chance.guid();
   let issueStatus = 'Open';
+  let issueName = document.getElementById('issueNameInput').value;
 
-  const issue = {
-    id: issueId,
-    description: issueDesc,
-    type: issueSeverity,
-    assignedTo: issueAssignedTo,
-    status: issueStatus
-  }
+  var issue = new Issue(issueName, issueType, issueDesc, issueStatus, issueAssignedTo);
 
-  if (localStorage.getItem('issues') == null) {
+
+  console.log(issue);
+
+  if (sessionStorage.getItem('issues') == null) {
     let issues = [];
     issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
+    sessionStorage.setItem('issues', JSON.stringify(issues));
   } else {
-    let issues = JSON.parse(localStorage.getItem('issues'));
+    let issues = JSON.parse(sessionStorage.getItem('issues'));
     issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
+    sessionStorage.setItem('issues', JSON.stringify(issues));
   }
 
   document.getElementById('issueInputForm').reset();
 
   fetchIssues();
 
-  e.preventDefault();
 }
 
 function setStatusClosed(id) {
-  let issues = JSON.parse(localStorage.getItem('issues'));
+  let issues = JSON.parse(sessionStorage.getItem('issues'));
 
   for (let i = 0; i < issues.length; i++) {
     if (issues[i].id == id) {
@@ -41,13 +86,13 @@ function setStatusClosed(id) {
     }
   }
 
-  localStorage.setItem('issues', JSON.stringify(issues));
+  sessionStorage.setItem('issues', JSON.stringify(issues));
 
   fetchIssues();
 }
 
 function deleteIssue(id) {
-  let issues = JSON.parse(localStorage.getItem('issues'));
+  let issues = JSON.parse(sessionStorage.getItem('issues'));
 
   for (var i = 0; i < issues.length; i++) {
     if (issues[i].id == id) {
@@ -55,14 +100,14 @@ function deleteIssue(id) {
     }
   }
 
-  localStorage.setItem('issues', JSON.stringify(issues));
+  sessionStorage.setItem('issues', JSON.stringify(issues));
 
   fetchIssues();
 }
 // This function will be used to fetch the list of issues
 
 function fetchIssues() {
-  let issues = JSON.parse(localStorage.getItem('issues'));
+  let issues = JSON.parse(sessionStorage.getItem('issues'));
   let issuesList = document.getElementById('issuesList');
 
   issuesList.innerHTML = '';
@@ -78,7 +123,7 @@ function fetchIssues() {
                               '<h6>Issue ID: ' + id + '</h6>'+
                               '<p><span class="label label-info">' + status + '</span></p>'+
                               '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + type + '</p>'+
                               '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
                               '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
                               '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
